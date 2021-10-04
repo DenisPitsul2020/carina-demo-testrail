358df85f0d02a65a634860150ccfe564a4b49778
Kirill of Turov <Kirill.of.Turov@gmail.com>
2021-10-04 19:42:40 +0300

    Integrated with test run and milestones TestRail

--- src/main/java/com/gurock/testrail/TestRailManager.java
+++ src/main/java/com/gurock/testrail/TestRailManager.java
@@ -6,10 +6,10 @@ import java.util.Map;
 
 public class TestRailManager {
 
-    public static final String TEST_RUN_ID = "17";
+    public static final String TEST_RUN_ID = "2";
     public static final String TESTRAIL_USERNAME = "dpitsul@solvd.com";
-    public static final String TESTRAIL_PASSWORD = "Denia261298";
-    public static final String RAILS_ENGINE_URL = "https://opendata.testrail.net/";
+    public static final String TESTRAIL_PASSWORD = "Solvd1234";
+    public static final String RAILS_ENGINE_URL = "https://solvd.testrail.io/";
     public static final int TEST_CASE_PASSED_STATUS = 1;
     public static final int TEST_CASE_FAILED_STATUS = 5;
 
@@ -24,6 +24,29 @@ public class TestRailManager {
         data.put("status_id", status);
         data.put("comment", "Test Executed - Status updated automatically from selenium test automation");
         client.sendPost("add_result_for_case/" + TEST_RUN_ID + "/" + testCaseId + "", data);
+    }
+
+    public static void addMilestone(String milestoneName) throws IOException, APIException {
+        APIClient client = new APIClient(RAILS_ENGINE_URL);
+        client.setUser(TESTRAIL_USERNAME);
+        client.setPassword(TESTRAIL_PASSWORD);
+
+        Map<String, Object> dataMilestone = new HashMap<>();
+        // dataMilestone.put("name", milestoneName);
+        dataMilestone.put("is_completed", true);
+        // client.sendPost("add_milestone/1", dataMilestone);
+        client.sendPost("update_milestone/1", dataMilestone);
+    }
+
+    public static void addTestRun(String runName) throws IOException, APIException {
+        APIClient client = new APIClient(RAILS_ENGINE_URL);
+        client.setUser(TESTRAIL_USERNAME);
+        client.setPassword(TESTRAIL_PASSWORD);
+
+        Map<String, Object> dataRun = new HashMap<>();
+        dataRun.put("name", runName);
+        dataRun.put("milestone_id", "11");
+        client.sendPost("add_run/1", dataRun);
     }
 
 }
--- src/test/java/com/qaprosoft/carina/demo/BaseTest.java
+++ src/test/java/com/qaprosoft/carina/demo/BaseTest.java
@@ -2,6 +2,7 @@ package com.qaprosoft.carina.demo;
 
 import com.gurock.testrail.TestRailManager;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 
 public abstract class BaseTest {
@@ -15,5 +16,11 @@ public abstract class BaseTest {
         } else if (result.getStatus() == ITestResult.FAILURE) {
             TestRailManager.addResultForTestCases(testCaseId, TestRailManager.TEST_CASE_FAILED_STATUS, result.getThrowable().getMessage());
         }
+    }
+
+    @AfterClass
+    public void addMilestone() throws Throwable {
+        TestRailManager.addMilestone("1.1");
+        TestRailManager.addTestRun("Test Run 10/4/2021 2");
     }
 }
